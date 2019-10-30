a1a55d1ef117dde2423201b3f5fee138e36eccc9
WalkingCake <mishayooo@mail.ru>
2019-10-30 21:14:57 +0500

    AnyAnswerEvent tests

---
+++ src/Event/AnyAnswerEventTest.java
@@ -0,0 +1,37 @@
+package Event;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class AnyAnswerEventTest {
+
+    private AnyAnswerEvent event;
+
+    public AnyAnswerEventTest() {
+        event = new AnyAnswerEvent("testID", "testName", "testText",
+                new Answer[]{new Answer("default", "testID_default", new HashMap<String, String>()),
+                new Answer("not_default", "testID_not_default", new HashMap<String, String>())}, false, false);
+    }
+
+    @Test
+    public void replyDefaultTest() {
+        String actual = event.reply("foo");
+        assertEquals("testID_default", actual);
+    }
+
+
+    @Test
+    public void replySpecialTest() {
+        String actual = event.reply("not_default");
+        assertEquals("testID_not_default", actual);
+    }
+
+    @Test
+    public void testToString() {
+        String actual = event.toString();
+        assertEquals("testText", actual);
+    }
+}
