085e96e9644fe6364188a772ce56654d816b7790
IlyaRyzhenkov <ilya.rigenkov2@gmail.com>
2019-11-10 12:10:15 +0500

    added telegram echo-bot

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,40 @@ import Game.Game;
 import Game.ConsoleIO;
 import SaveLoader.ISaveLoader;
 import SaveLoader.JSONsaveLoader;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
+import TelegramBot.Bot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
 
     public static void main(String[] args) {
+        if (true) {
+            runTelegram();
+        }
+        else {
+            runConsole();
+        }
+    }
+
+    private static void runConsole() {
         ConsoleIO console = new ConsoleIO();
         ILoader storage = new EventStorage();
         ISaveLoader loader = new JSONsaveLoader();
 
         Game game = new Game(console, storage, loader);
         game.startGameAtID("Main menu/menu");
+    }
+
+    private static void runTelegram() {
+        ApiContextInitializer.init();
+        
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/TelegramBot/Bot.java
@@ -0,0 +1,40 @@
+package TelegramBot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private String botUsername = "qaqaqaqaqaqaqabot";
+    private String botToken = "1018946407:AAFmEI4OIN_OOL0O3chCqgGl7FCrsKRHQEU";
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    private synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.err.print("error sending message");
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
